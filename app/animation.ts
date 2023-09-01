4766c5da8696a35133fca62209f58ebb75d2531c
BluTargaryan <opeyemiyolo@gmail.com>
2023-09-01 20:03:07 +0100

    Next mini work used to learn framer and tailwind

---
+++ app/animation.ts
@@ -0,0 +1,19 @@
+export const container={
+    hidden:{opacity:0},
+    show:{
+        opacity:1,
+        transition:{
+            delayChildren:.5,
+            staggerChildren:.25,
+        }
+    }
+}
+
+export const item={
+    hidden:{y:'100%'},
+    show:{y:'0%', 
+    transition:{
+        duration:.5,
+    }
+}
+}
